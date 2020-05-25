b057147a71e2b8ad6042110bcd52e0fc819d03bb
KibouAru <65344263+KibouAru@users.noreply.github.com>
2020-05-25 18:53:42 +0500

    v.1.2

--- js/script.js
+++ js/script.js
@@ -1,88 +1,161 @@
+// Проверяет на случай если ответы не выбраны
+function noneChousen(exs) {
+  var int = eval(exs).amountOfAnswers;
+  var itr = 1;
+
+  do  {
+    if ($("#"+exs+"Answer"+String(itr)).prop('checked') == false) {
+      // $("#"+exs+"Answer"+String(itr)+"-label").css("backgroundColor","grey"); debug
+      itr++;
+    }
+    else {
+      break;
+    }
+  } while ((itr-1) <= int);
+
+
+  if ((itr-1) == int) {
+    return 1;
+  }
+  else {
+    return 0;
+  }
+}
+
+// В зависимости от правильности ответа выделить все тусклым цветом, затем выделить выбранные ярким
+function highlightAnswersT1(exs) {
+  var int = eval(exs).amountOfAnswers;
+  var exsVar = eval(exs);
+  var count = 1
+   // шаг 1
+    do {
+      if ('#' + exs + 'Answer' + count == "#" + exsVar.correctAnswerId)   {
+        $('#' + exs + 'Answer' + count + "-label").css("backgroundColor","rgba(131, 192, 160, 0.5)")
+      }
+      else {
+        $('#' + exs + 'Answer' + count + "-label").css("backgroundColor","rgba(192, 131, 131,0.5)")
+      }
+      count++
+    } while (count <= int)
+
+    // шаг 2
+    count = 1;
+
+    do {
+      if ('#' + exs + 'Answer' + count == "#" + $('.'+exs+'Answers[type=radio]:checked').attr("id"))   {
+        if ('#' + exs + 'Answer' + count == "#" + exsVar.correctAnswerId)   {
+          $('#' + exs + 'Answer' + count + "-label").css("backgroundColor","rgba(131, 222, 147, 1)");
+          $('#' + exs + 'Answer' + count + "-label").css("color", "rgb(24,24,24)");
+          break;
+        }
+        else {
+          $('#' + exs + 'Answer' + count + "-label").css("backgroundColor","rgba(227, 66, 66, 1)");
+          $('#' + exs + 'Answer' + count + "-label").css("color", "rgb(24,24,24)");
+          break;
+        }
+      }
+      else {
+        count++
+      }
+    } while (count <= int)
+}
+// данные заданий ------------
+
 var exs1 = {
-  condition: 0,
+  condition: "unknown",
   correctAnswerId: "exs1Answer1",
-  type: "1a"
+  maxScore: 1,
+  amountOfAnswers: 4
 }
 var exs2 = {
-  condition: 0,
+  condition: "unknown",
   correctAnswerId: "exs2Answer4",
-  type: "1a"
+  maxScore: 1,
+  amountOfAnswers: 4
 }
-// var exs3 = {
-//   condition: 0,
-//   correctAnswerId: "exs1Answer1",
-//   type: "1a"
+var exs3 = {
+  condition: "unknown",
+  correctAnswersId: ["exs3Answer1","exs3Answer3"],
+}
+
+//
+//
+//
+
+
+// switch () {
+//   case value1: code
+//   break;
+//   case value2: code
+//   break;
+//   default:
 // }
-// var exs4 = {
-//   condition: 0,
-//   correctAnswerId: "exs1Answer1",
-//   type: "1a"
+
+
+
+// #1 all 1a types
+
+function checkExs1(exs,forced) {
+
+    var exsVar = eval(exs);
+
+      if (noneChousen(exs) == true && forced == false) {}
+      else {
+      if (noneChousen(exs) == true && forced == true) {
+          exsVar.condition = 0;
+      }
+      else if ($("#"+exsVar.correctAnswerId).is(':checked')) {
+          exsVar.condition = 1;
+          }
+        else {
+          exsVar.condition = 0
+          }
+          highlightAnswersT1(exs)
+          $("input."+exs+"Answers").attr("disabled", true);
+      }
+}
+
+// #2
+
+// function checkExs2(forced) {
+//
+//       if (noneChousen("exs2") == true && forced == false) {}
+//       else {
+//       if (noneChousen("exs2") == true && forced == true) {
+//           exs2.condition = 0;
+//       }
+//       else if ($("#"+exs2.correctAnswerId).is(':checked')) {
+//           exs2.condition = 1;
+//           }
+//         else {
+//           exs2.condition = 0
+//           }
+//           highlightAnswersT1("exs2")
+//           $("input.exs2Answers").attr("disabled", true);
+//       }
 // }
-
-function checkExs(exs) {
-  if ($("#exs1Answer1").prop('checked') == false && $("#exs1Answer2").prop('checked') == false && $("#exs1Answer3").prop('checked') == false && $("#exs1Answer4").prop('checked') == false) {
-  }
-  else if (document.getElementById(exs.correctAnswerId).checked) {
-      $("input.exs1Answers").attr("disabled", true);
-      exs.condition = 2;
-      $("#" + exs.correctAnswerId + "-label").css("backgroundColor", "green");
-      $("#" + exs.correctAnswerId + "-label").css("color", "rgb(24,24,24)");
-      }
-    else {
-      $("input.exs1Answers").attr("disabled", true);
-      exs.condition = 0
-      var selectedAnswer = "#" + $('.exs1Answers[type=radio]:checked').attr("id") + "-label";
-      $(selectedAnswer).css("backgroundColor", "red");
-      $(selectedAnswer).css("color", "rgb(24,24,24)");
-    }
-}
-
-
-//---------------------------
-
-
-function checkExs2() {
-  if ($("#exs2Answer1").prop('checked') == false && $("#exs2Answer2").prop('checked') == false && $("#exs2Answer3").prop('checked') == false && $("#exs2Answer4").prop('checked') == false) {
-  }
-  else if (document.getElementById(exs2.correctAnswerId).checked) {
-      $("input.exs1Answers").attr("disabled", true);
-      exs2.condition = 2;
-      $("#" + exs2.correctAnswerId + "-label").css("backgroundColor", "green");
-      $("#" + exs2.correctAnswerId + "-label").css("color", "rgb(24,24,24)");
-      }
-    else {
-      $("input.exs2Answers").attr("disabled", true);
-      exs2.condition = 0
-      var selectedAnswer = "#" + $('.exs2Answers[type=radio]:checked').attr("id") + "-label";
-      $(selectedAnswer).css("backgroundColor", "red");
-      $(selectedAnswer).css("color", "rgb(24,24,24)");
-    }
-}
-
 
 
 
 
 
 //-----------------------------------
-// type 1b example chouse-several-answers
-
-var exs3 = {
-  condition: 0,
-  correctAnswersId: ["exs3Answer1","exs3Answer3"],
-  type: "1b"
-}
+// type 1b
 
 
-function checkExs3() {
+
+
+function checkExs3(forced) {
     var selectedAnswers = $('input[type=checkbox]:checked').map(function() {
         return $(this).attr('id');
     }).get();
 
 
 
-    if ($("#exs3Answer1").prop('checked') == false && $("#exs3Answer2").prop('checked') == false && $("#exs3Answer3").prop('checked') == false && $("#exs3Answer4").prop('checked') == false) {
-
+    if (noneChousen("exs3") == true) {}
+    else if (forced == true) {
+        exs3.condition = 0;
+        $("input.exs3Answers").attr("disabled", true);
     }
     else if (JSON.stringify(selectedAnswers)==JSON.stringify(exs3.correctAnswersId)) {
 
@@ -120,11 +193,68 @@ function checkExs3() {
 
 
 
-  function checkAll() {
+
+    function disableUnchecked() {
+      if (exs1.condition == "unknown") {checkExs1('exs1',true)}
+      if (exs2.condition == "unknown") {checkExs1('exs2',true)}
+      if (exs3.condition == "unknown") {checkExs3(true)}
+    }
+
+    function displayResult() {
+      $("#exs1Result span span").text(exs1.condition)
+      $("#exs2Result span span").text(exs2.condition)
+      $("#exs3Result span span").text(exs3.condition)
+    }
+
+    // function colorResults() {
+    //   switch (exs1.condition) {
+    //     case 2: $("#exs1Result").css("backgroundColor","green");
+    //     case 1: $("#exs1Result").css("backgroundColor","yellow");
+    //     case 0: $("#exs1Result").css("backgroundColor","red");
+    //     break;
+    //   }
+    //   switch (exs2.condition) {
+    //     case 2: $("#exs2Result").css("backgroundColor","green");
+    //     case 1: $("#exs2Result").css("backgroundColor","yellow");
+    //     case 0: $("#exs2Result").css("backgroundColor","red");
+    //     break;
+    //   }
+    //   switch (exs3.condition) {
+    //     case 2: $("#exs3Result").css("backgroundColor","green");
+    //     case 1: $("#exs3Result").css("backgroundColor","yellow");
+    //     case 0: $("#exs3Result").css("backgroundColor","red");
+    //     break;
+    //   }
+    // }
+
+  function checkAllButton() {
     $('.submission-container').fadeOut(200);
-    $('.results-box').slideDown(500,'linear');
+    $('.results-box').slideDown(300,'linear');
     $('html,body').animate({ scrollTop: 2000 }, 'slow')
+
+    disableUnchecked();
+    displayResult();
+    // colorResults();
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
